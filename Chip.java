f713997810d0e6f18dd7fc16bdb8b57076830c99
Jenrick Lim <jenrick_lim@dlsu.edu.ph>
2025-10-05 21:21:44 +0800

    feat: added methods in Chips Class

--- Chip.java
+++ Chip.java
@@ -1,3 +1,29 @@
 public class Chip {
     
+    private int x;
+    private int y;
+    private final Inventory INVENTORY;  
+    
+    public Chip(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+        this.INVENTORY = new Inventory();  // start empty
+    }
+
+    void move(char direction, Maps maps) {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+   
+    public Inventory getInventory() {
+        return INVENTORY;
+    }
+
 }
