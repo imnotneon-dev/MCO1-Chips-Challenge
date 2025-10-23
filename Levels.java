7b53a2431a6fc9331a95acc14e3ce76f9a103987
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-10-23 21:24:45 +0800

    feat: added javadoc

--- Levels.java
+++ Levels.java
@@ -1,19 +1,19 @@
-/*
+/**
  * This Levels class handles the layout of the maps inside the game. It currently has 3 levels and it is used by the Display class to show the map layouts and
  * it is the one to show the tile characters, hazard(water/fire) tiles, doors, force tiles, collectibles, and exit tile.
  */
 
 public class Levels {
-    /*
+    /**
      * 2d array to use for the layout, it is the one responsible to display the tile layout
      */
     private char[][] map;
-    /*
+    /**
      * Contains the chips required to pass a level
      */
     private int chips;
 
-    /*
+    /**
      * Constructor of Levels class that accepts the given map and chips required to pass the level/map
      * 
      * @param map - 2d array for the map layout
@@ -24,11 +24,11 @@ public class Levels {
         this.chips = chips;
     }
     
-    /*
+    /**
      * Current layout for the first map, it is a beginner and teaching level as it only features keys and door tiles
      * Currently has a red key, red door, blue key, blue door, and 4 chips to collect to pass the level
      * 
-     * @return - it returns the current map layout for level 1 for the Display class to use
+     * @return Levels- it returns the current map layout for level 1 for the Display class to use
      */
     public static Levels generateMap1() {
         char[][] grid = {
@@ -48,11 +48,11 @@ public class Levels {
         return new Levels(grid, chips);
     }
 
-    /*
+    /**
      * Current layout for the second map, it is a normal level as it features hazard and force tiles
      * Currently has force tiles, flippers, water tiles, fire boots, fire tiles, and 4 chips to pass the level
      * 
-     * @return - returns the current map layout for level 2 for the Display class to use
+     * @return Levels - returns the current map layout for level 2 for the Display class to use
      */
     public static Levels generateMap2() {
         char[][] grid = {
@@ -72,11 +72,11 @@ public class Levels {
         return new Levels(grid, chips);
     }
 
-    /*
+    /**
      * Current layout for the third map, it is a combination of difficulty for past levels as it features both hazard and force tiles and key and door tiles
      * This map currently has force tiles, flippers, water tiles, fire boots, fire tiles, red key, red door, blue key, blue door and 5 chips to pass the level
      * 
-     * @return - returns the current map layout for level 3 for the Display class to use
+     * @return Levels- returns the current map layout for level 3 for the Display class to use
      */
     public static Levels generateMap3() {
         char[][] grid = {
@@ -98,25 +98,25 @@ public class Levels {
         return new Levels(grid, chips);
     }
 
-    /*
+    /**
      * Returns the map layout being used
      * 
-     * @return - the map layout being used
+     * @return map - the map layout being used
      */
     public char[][] getMap() {
         return map;
     }
 
-    /*
+    /**
      * Returns the chips required for the map being used
      * 
-     * @return - the chips required to pass the current map level
+     * @return chips - the chips required to pass the current map level
      */
     public int getChips() {
         return chips;
     }
 
-    /*
+    /**
      * Returns the current chips placed on the map, used to simulate passing a level when all the chips
      * has been collected
      * 
