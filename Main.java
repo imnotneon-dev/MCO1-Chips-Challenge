eca2a2a4297f77e160ac347fd72ed5ee67b98d01
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-10-24 10:09:27 +0800

    fix: added additional documentation

--- Main.java
+++ Main.java
@@ -1,25 +1,22 @@
 /**
- * Main class for the Chips Challenge application.
- * * @author Jenrick Lim, Ryan Malapitan (S16)
+ * Main Class / Program
+ * 
+ * - For running the Chips Challenge application.
+ * @author Jenrick Lim, Ryan Malapitan (S16)
  */
 public class Main {
+
     /**
      * The main static void class to run the program and the different functions of it
      * 
-     * * @param args - accepts the argument of strings to simulate state of the program
+     * @param args - accepts the argument of strings to simulate state of the program
      */    
     public static void main(String[] args) {
-        /**
-         * Creates a new Display instance that is necessary for the menu and map layout and game state display for the user to see
-         */
+        // Creates a new Display instance that is necessary for the menu and map layout and game state display for the user to see
         Display menu = new Display();
-        /**
-         * Creates a new Controller instance that is necessary for the gameplay of the player mainly chips movement and interaction with levels
-         */
+        //Creates a new Controller instance that is necessary for the gameplay of the player mainly chips movement and interaction with levels
         Controller controller = new Controller();
-        /**
-         * Calls the controller method and shows the main menu to simulate the start of the program for the player
-         */
+        //Calls the controller method and shows the main menu to simulate the start of the program for the player
         menu.showMainMenu(controller);
     }
 }
